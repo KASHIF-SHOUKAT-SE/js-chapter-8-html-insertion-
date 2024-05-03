73e673e9f18d47c177053258520c2287432577a7
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-05-03 11:36:26 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log("hey i am kashif")
+// let a = document.getElementsByTagName("div")[0]
+// a.innerHTML = a.innerHTML + '<h1>Hello World!';
+
+// let div = document.createElement('div')
+// div.innerHTML = '<h1>kashif</h1>'
+// a.append(div);
+// a.prepend(div);
+// a.before(div);
+// a.after(div);
+// a.replaceWith(div);
+
+first.insertAdjacentHTML('beforebegen','<div class="test">beforebegin</div>');
+first.insertAdjacentHTML('beforeend','<div class="test">beforebegin</div>');
+first.insertAdjacentHTML('afterbegin','<div class="test">beforebegin</div>');
+first.insertAdjacentHTML('afterend','<div class="test">beforebegin</div>');
+
